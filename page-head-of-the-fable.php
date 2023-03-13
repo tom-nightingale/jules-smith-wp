103bfa3093bd6a38b5035b4c1881c76eabab5f0d
Tom Nightingale <tom@weareleague.co.uk>
2023-03-13 21:28:20 +0000

    feat(HOF): Start to build out page

--- page-head-of-the-fable.php
+++ page-head-of-the-fable.php
@@ -4,5 +4,11 @@ $context = Timber::context();
 $timber_post = new Timber\Post();
 $context['post'] = $timber_post;
 
+$terms = get_terms( array(
+    'taxonomy' => 'book-categories',
+    'hide_empty' => false,
+) );
+
+$context['book_categories'] = $terms;
 
 Timber::render( [ 'page-'.$timber_post->slug.'.twig', 'page.twig' ], $context );
