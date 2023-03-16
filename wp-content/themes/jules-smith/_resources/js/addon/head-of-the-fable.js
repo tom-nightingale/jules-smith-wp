cf613e1faef2b653ff51faee34616e834e6ed9bf
Tom Nightingale <tom@weareleague.co.uk>
2023-03-16 18:26:11 +0000

    feat(BookFilters): Remember book filter

--- wp-content/themes/jules-smith/_resources/js/addon/head-of-the-fable.js
+++ wp-content/themes/jules-smith/_resources/js/addon/head-of-the-fable.js
@@ -1,33 +1,59 @@
 const filters = document.querySelectorAll('.book-filter');
 const books = document.querySelectorAll('.book-link');
 
-//set the first filter as active
-filters[0].classList.remove('inactive');
-filters[0].classList.add('active');
 
-const resetActiveState = () => {
+const resetActiveState = (savedFilter) => {
     filters.forEach((filter) => {
-        filter.classList.remove('active');
-        filter.classList.add('inactive');
+        if(filter.dataset.category !== savedFilter) {
+            filter.classList.remove('active');
+            filter.classList.add('inactive');
+        }
+        else {
+            filter.classList.remove('inactive');
+            filter.classList.add('active');
+        }
     });
 }
 
-const startFilters = () => {
+const startFilters = (savedFilter) => {
     books.forEach((book) => {
         book.classList.add('hidden');
-        const slug = filters[0].dataset.category;
+        let slug;
+        if(savedFilter) {
+            slug = savedFilter;
+        }
+        else {
+            slug = filters[0].dataset.category;
+        }
         if(book.dataset.category === slug) {
             book.classList.remove('hidden');
         }
     })
 }
 
-startFilters();
+const savedFilter = localStorage.getItem('activeCategory');
+
+// console.log(localStorage.getItem('activeCategory') === null);
+if(savedFilter === null) {
+    console.log('activeFilters is null');
+    filters[0].classList.remove('inactive');
+    filters[0].classList.add('active');
+}
+else {
+    resetActiveState(savedFilter);
+    startFilters(savedFilter);
+}
+
 
 filters.forEach((filter) => {
     filter.addEventListener('click', (e) => {
         e.preventDefault();
         const slug = filter.dataset.category;
+        
+        // console.log(slug);
+        // Store in localStorage
+        localStorage.setItem('activeCategory', slug);
+        // console.log(localStorage.getItem('activeCategory'));
 
         // reset active state
         resetActiveState();
